1ad19bb5bb85504c47b5c601b2dda794fee6a021
Смолькин Владислав <vladislav.smolkin@gazprombank.ru>
2024-11-06 19:07:39 +0300

    feat: added containerized frontend on react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 8000,
+     watch: {
+       usePolling: true
+     }
+  },
   plugins: [react()],
 })
+
